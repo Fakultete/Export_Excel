c9ef29c583120d59273033e77a5efb41f823061c
buttercup01 <zoja.meh311@gmail.com>
2023-01-05 18:24:36 +0100

    Ustvarjena GUI Form-a za vnos podatkov

---
+++ src/database/Fakultete.java
@@ -0,0 +1,20 @@
+package database;
+
+import javax.swing.*;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class Fakultete extends JFrame {
+    private JTable fakultete;
+    private JButton VNESIButton;
+    private JButton POSODOBIButton;
+    private JButton IZBRISIButton;
+    private JTextField ime;
+    private JTextField kljucna_beseda;
+    private JTextField opis;
+    private JTextField kraj_id;
+    private JPanel Form;
+}
+
