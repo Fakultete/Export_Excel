145883c5377bd8a1414ab118013ef0cc221c13a9
buttercup01 <zoja.meh311@gmail.com>
2023-01-09 22:15:49 +0100

    Ustvarjeni Formi za LOGIN in REGISTRACIJO

---
+++ src/database/Login.java
@@ -0,0 +1,11 @@
+package database;
+
+import javax.swing.*;
+
+public class Login {
+    private JTextField textField1;
+    private JButton PRIJAVAButton;
+    private JButton REGISTRIRAJSEButton;
+    private JLabel label1;
+    private JPasswordField passwordField1;
+}
---
+++ src/database/Register.java
@@ -0,0 +1,13 @@
+package database;
+
+import javax.swing.*;
+
+public class Register {
+    private JPanel Panel;
+    private JLabel label1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JButton REGISTRIRAJSEButton;
+    private JButton nazajNaLOGINButton;
+}
