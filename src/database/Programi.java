b8a5c449fd20d1dd837308cc5dbb76c03b200d15
buttercup01 <zoja.meh311@gmail.com>
2023-01-09 21:36:30 +0100

    INSERT, UPDATE, DELETE nad tabelo "programi"

--- src/database/Programi.java
+++ src/database/Programi.java
@@ -4,8 +4,11 @@ import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.*;
 public class Programi {
     public JPanel Form;
@@ -47,7 +50,7 @@ public class Programi {
     }
     void table_load() {
         try {
-            pst = con.prepareStatement("SELECT p.id, p.ime, s.naziv, f.ime FROM programi p INNER JOIN stopnje s ON s.id=p.stopnja_id INNER JOIN fakultete f ON f.id=p.fakulteta_id ORDER BY p.id;");
+            pst = con.prepareStatement("SELECT p.id, p.ime, p.opis, s.naziv, f.ime FROM programi p INNER JOIN stopnje s ON s.id=p.stopnja_id INNER JOIN fakultete f ON f.id=p.fakulteta_id ORDER BY p.id;");
             ResultSet rs = pst.executeQuery();
             programi.setModel(DbUtils.resultSetToTableModel(rs));
         } catch (SQLException e) {
@@ -91,6 +94,86 @@ public class Programi {
                 }
             }
         });
+        //OB KLIKU V TABELI NA VRSTICO
+        programi.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                DefaultTableModel RecordTable = (DefaultTableModel)programi.getModel();
+                int SelectedRows = programi.getSelectedRow();
+
+                txtime.setText(RecordTable.getValueAt(SelectedRows,1).toString());
+                txtopis.setText(RecordTable.getValueAt(SelectedRows,2).toString());
+                combostopnja.setSelectedItem(RecordTable.getValueAt(SelectedRows,3).toString());
+                combofakulteta.setSelectedItem(RecordTable.getValueAt(SelectedRows,4).toString());
+            }
+        });
+        /**
+         * UPDATE
+         */
+        POSODOBIButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                DefaultTableModel RecordTable = (DefaultTableModel)programi.getModel();
+                int SelectedRows = programi.getSelectedRow();
+                int auto_id=Integer.parseInt(RecordTable.getValueAt(SelectedRows, 0).toString());
+
+                String ime_p, opis_p, stopnja_p, fakulteta_p;
+                ime_p = txtime.getText();
+                opis_p = txtopis.getText();
+                stopnja_p = combostopnja.getSelectedItem().toString();
+                fakulteta_p = combofakulteta.getSelectedItem().toString();
+
+                try {
+                    pst = con.prepareStatement("SELECT update_programi(?, ?, ?, ?, ?);");
+                    pst.setInt(1, auto_id);
+                    pst.setString(2, ime_p);
+                    pst.setString(3, opis_p);
+                    pst.setString(4, stopnja_p);
+                    pst.setString(5, fakulteta_p);
+
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "USPEŠNO POSODOBLJENO!");
+                    table_load();
+                    txtime.setText("");
+                    txtopis.setText("");
+                    combostopnja.setSelectedItem("");
+                    combofakulteta.setSelectedItem("");
+                }
+                catch (SQLException e1)
+                {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        /**
+         * DELETE
+         */
+        IZBRISIButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultTableModel RecordTable = (DefaultTableModel)programi.getModel();
+                int SelectedRows = programi.getSelectedRow();
+                int auto_id=Integer.parseInt(RecordTable.getValueAt(SelectedRows, 0).toString());
+
+                try {
+                    pst = con.prepareStatement("SELECT delete_programi(?)");
+                    pst.setInt(1, auto_id);
+
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "USPEŠNO STE IZBRISALI PROGRAM!");
+                    table_load();
+                    txtime.setText("");
+                    txtopis.setText("");
+                    combostopnja.setSelectedItem("");
+                    combofakulteta.setSelectedItem("");
+                }
+                catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
         NAZAJNAFAKULTETEButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -104,6 +187,86 @@ public class Programi {
                     con.close();
                 } catch (SQLException ex) {
                     throw new RuntimeException(ex);
+                }
+            }
+        });
+        combostopnja.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                try {
+                    pst = con.prepareStatement("SELECT DISTINCT s.naziv FROM stopnje s LEFT OUTER JOIN programi p ON s.id=p.stopnja_id;");
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try{
+                    ResultSet rs = pst.executeQuery();
+                    while(rs.next()) {
+                        String abc = rs.getString(1);
+                        combostopnja.addItem(abc);
+                    }
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        combofakulteta.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                try {
+                    pst = con.prepareStatement("SELECT DISTINCT ime FROM fakultete;");
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try{
+                    ResultSet rs = pst.executeQuery();
+                    while(rs.next()) {
+                        String abc = rs.getString(1);
+                        combofakulteta.addItem(abc);
+                    }
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        OSVEZIButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                table_load();
+            }
+        });
+        iscifakulteta.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                try {
+                    pst = con.prepareStatement("SELECT DISTINCT ime FROM fakultete;");
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try{
+                    ResultSet rs = pst.executeQuery();
+                    while(rs.next()) {
+                        String abc = rs.getString(1);
+                        iscifakulteta.addItem(abc);
+                    }
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        ISCIButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String r = iscifakulteta.getSelectedItem().toString();
+                    pst = con.prepareStatement("SELECT p.id, p.ime, p.opis, s.naziv, f.ime FROM programi p INNER JOIN stopnje s ON s.id=p.stopnja_id INNER JOIN fakultete f ON f.id=p.fakulteta_id WHERE f.ime=? ORDER BY p.id;");
+                    pst.setString(1, r);
+                    ResultSet rs = pst.executeQuery();
+                    programi.setModel(DbUtils.resultSetToTableModel(rs));
+                } catch (SQLException r) {
+                    r.printStackTrace();
                 }
             }
         });
